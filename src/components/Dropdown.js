8412294df834d79fc232c6eb9beb8dd02148aab6
Dhruv Mohindru <dmohindru@gmail.com>
2023-02-13 19:33:52 +1100

    Finished video 196

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,35 @@
-function Dropdown({ options }) {
+import { useState } from "react";
+function Dropdown({ options, selection, onSelect }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const handleOptionClick = (option) => {
+        // Close Dropdown
+        setIsOpen(false);
+        // What option did the user click on
+        onSelect(option);
+    }
+
+    const renderedOptions = options.map((option) => {
+        return <div onClick={() => handleOptionClick(option)}
+        key={option.value}>
+            {option.label}
+        </div>
+    })
+
+    let content = 'Select...';
+    if (selection) {
+        content = selection.label;
+    }
     
     return (
         <div>
-            Dropdown ...
+            <div onClick={handleClick}>{content}</div>
+            {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
 }
