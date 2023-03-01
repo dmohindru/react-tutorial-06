7b05852a125923cd471250757c05cac44a31edd2
Dhruv Mohindru <dmohindru@gmail.com>
2023-03-02 07:10:57 +1100

    Compeleted video 249

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,23 @@
 import Table from "../components/Table";
 
 function TablePage() {
+    const data = [
+        { name: 'Orange', color: 'bg-orange-500', score: 5},
+        { name: 'Apple', color: 'bg-red-500', score: 3},
+        { name: 'Banana', color: 'bg-yellow-500', score: 1},
+        { name: 'Lime', color: 'bg-green-500', score: 4}
+    ];
+
+    const config = [
+        { label: 'Fruits' },
+        { label: 'Color' },
+        { label: 'Score' }
+
+    ];
+
     return (
         <div>
-            <Table />
+            <Table data={data} config={config} />
         </div>
     );
 }
