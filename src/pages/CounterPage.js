2c1a771c64de43985f6daca8d7edc2e97ef3abaa
Dhruv Mohindru <dmohindru@gmail.com>
2023-03-09 19:36:51 +1100

    Completed section 18

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import Button from '../components/Button';
 import { useReducer } from 'react';
 import Panel from '../components/Panel';
@@ -9,6 +10,8 @@ const DECREMENT_COUNT = 'decrement';
 const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 
+/*
+// Important: Code example of reducer with using immer library
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
@@ -40,16 +43,49 @@ const reducer = (state, action) => {
       //throw new Error('unexpected action type: ' + action.type);
   }
     
+};*/
+
+// Code example of reducer using immer library
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;  
+      return; 
+
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+      
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+
+    default:
+      return;  
+      //throw new Error('unexpected action type: ' + action.type);
+  }
+    
 };
 
 
 function CounterPage({initialCount}) {
     // const [count, setCount] = useState(initialCount);
     // const [valueToAdd, setValueToAdd] = useState(0);
-    const [state, dispatch] = useReducer(reducer, {
-        count: initialCount,
-        valueToAdd: 0 
-    });
+    // IMPORTANT: Code example without immer library
+    // const [state, dispatch] = useReducer(reducer, {
+    //     count: initialCount,
+    //     valueToAdd: 0 
+    // });
+
+    const [state, dispatch] = useReducer(produce(reducer), {
+      count: initialCount,
+      valueToAdd: 0 
+  });
 
     console.log(state);
     
